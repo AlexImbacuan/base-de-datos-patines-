59417e3a78169fb996a2f7cba2292ce319612722
AlexImbacuan <alex.imbacuan@correounivalle.edu.co>
2025-05-12 22:36:48 -0500

    modificacion funciones

--- electrica_functions.sql
+++ electrica_functions.sql
@@ -21,21 +21,21 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Función para calcular el costo de un alquiler en tiempo real
-CREATE OR REPLACE FUNCTION calcular_costo_actual(alquiler_id INT)
+CREATE OR REPLACE FUNCTION calcular_costo_actual(id_alqui INT)
 RETURNS DECIMAL(10,2) AS $$
 DECLARE
     tarifa_por_minuto DECIMAL(10,2);
     minutos_transcurridos INT;
     costo DECIMAL(10,2);
 BEGIN
-    SELECT t.valor_por_minuto INTO tarifa_por_minuto
+    SELECT t.costo_por_minuto INTO tarifa_por_minuto
     FROM alquileres a
     JOIN tarifas t ON a.tarifa_id = t.tarifa_id
-    WHERE a.alquiler_id = alquiler_id;
+    WHERE a.alquiler_id = id_alqui;
 
 	    SELECT EXTRACT(EPOCH FROM (NOW() - a.fecha_hora_inicio)) / 60 INTO minutos_transcurridos
     FROM alquileres a
-    WHERE a.alquiler_id = alquiler_id;
+    WHERE a.alquiler_id = id_alqui;
 
     costo := tarifa_por_minuto * minutos_transcurridos;
     RETURN ROUND(costo, 2);
@@ -43,12 +43,12 @@ END;
 $$ LANGUAGE plpgsql;
 	
 -- Función para verificar disponibilidad de una patineta
-CREATE OR REPLACE FUNCTION esta_disponible(patineta_id INT)
+CREATE OR REPLACE FUNCTION esta_disponible(patin_id INT)
 RETURNS BOOLEAN AS $$
 DECLARE
     estado INT;
 BEGIN
-    SELECT estado_id INTO estado FROM patinetas WHERE patineta_id = patineta_id;
+    SELECT estado_id INTO estado FROM patinetas WHERE patineta_id = patin_id;
     RETURN estado = 1;
 END;
 $$ LANGUAGE plpgsql;
